dcd0062c95ec3f4e8b4449e89de6276e0882910b
ncortes04 <nickapoleon@gmail.com>
2022-09-30 18:49:34 -0700

    added comments to the js

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ const options = {
     method: 'GET',
   };
 var APIKey="5f7a30057399336fce09fdf115bb9746";
-/* show future weather */
+
+/* runs mutiple functions with the city parameter which starts all the functions */
 function displayWeather(e){
     e.preventDefault();
     var city = inputEl.value
@@ -29,7 +30,7 @@ function displayWeather(e){
     setItems()
 }
 
-
+/* gets the weather displayed in the box with a fetch call*/
 function getWeather(city){
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&APPID=" + APIKey, options) 
         .then(data => data.json())
@@ -44,27 +45,7 @@ function getWeather(city){
       /* convert wind from meters per secoond to mph and display in HTML */
        currentWSpeed.innerHTML = (data.wind.speed * 2.237 + "mph")
        currentCity.innerHTML = data.name + (today.format(" (MM/DD/YYYY)"))
-
        forecast(data.id);
-       if(data.status == 200){
-          cityStore=JSON.parse(localStorage.getItem("cityname"));
-          console.log(cityStore);
-          if (cityStore==null){
-              cityStore=[];
-              cityStore.push(city.toUpperCase()
-              );
-              localStorage.setItem("cityname",JSON.stringify(cityStore));
-              addToList(city);
-          }
-          else {
-              if(find(city)>0){
-                  cityStore.push(city.toUpperCase());
-                  localStorage.setItem("cityname",JSON.stringify(cityStore));
-                  addToList(city);
-              }
-          }
-       }
-
     });
 }
 
@@ -76,46 +57,58 @@ function forecast(cityid) {
       .then(function (data) {
             console.log(data)
             for(i=0; i < 5; i++){
+            /* variables that grav each id i created on the html and uses a for loop to loop though each day */
             var tempId = document.getElementById('tempf'+ classes[i])
             var humidId = document.getElementById('humidityf'+ classes[i])
             var dateId = document.getElementById('datef'+ classes[i])
             var test =  document.getElementById('imgbox' + classes[i])
+            /* each day for weather is stored in an object called list and every 8 index's a new day is documented */
             var datalist = data.list[i*8]
+            /* shows the date for the weather */
             var date = datalist.dt_txt
+            /* grabs image from the data list */
             var img = document.getElementById("icon" + classes[i])
             var tempround = ((datalist.main.temp - 273.15)*9/5 + 32 ) 
+            /* adds class to the image */
             img.classList.add('image');
             img.src = ("src","https://openweathermap.org/img/wn/" + datalist.weather[0].icon + "@2x.png");
             test.appendChild(img);
-
+            /* shows temp and humidity */
             tempId.innerHTML =  "Temp: " + (Math.round(tempround))+ "&#176F";
             dateId.innerHTML = date
             humidId.innerHTML = "Humidity:" + datalist.main.humidity + "%"
             }
   });
 }
+/* creates button with city parameter which is later used by mutiple functions*/
 function createPastSearchBtn(city) {
     var pastSearchBtn = document.createElement('button');
-    // Update 'title' variable once this is officially created here based on the search input
     pastSearchBtn.textContent = city;
     pastSearchBtn.setAttribute('type', 'button');
     pastSearchBtn.classList.add('btn', 'button', 'btn-primary', 'mt-2', 'search-button');
     searchHistoryEl.append(pastSearchBtn);
     pastSearchBtn.addEventListener('click', searchHandlerPast);
+    /* if the used does not input nothing then run the push. Otherwise use can push empty space
+    into the array which created an empty button */
    if(inputEl.value !== ''){
     citiesArr.push(city)
    }
-    console.log(citiesArr)
   }
+  /* created button which grabs the texct content of that set button and runs as a parameter for get weather */
   function searchHandlerPast(event) {
     city = event.target.textContent;
     getWeather(city);
   }
-  function setItems(event){
+  /* function to set items which is called with the search button */
+  function setItems(){
+    /* setting items as in json and converts to a string so it can be accessed as an array */
     localStorage.setItem('Cities', JSON.stringify(citiesArr));
 };
+/* returns a string for the values associated with the key Cities*/
 var stored = JSON.parse(localStorage.getItem("Cities"));
 
+/* grabs the stored cities and creats buttons wiht thema nd pushes them into the citiesArr array.
+this is so when the used refreshes they cannot reset the value of the cities localstorage */
 function  renderLastRegistered(){
     for(let i=0; i <stored.length;  i++){
         city = stored[i]
@@ -124,16 +117,15 @@ function  renderLastRegistered(){
     }
  
 }
-
+/* this is necessary because if the local storage is empty it throws an erorr when running the command.
+i made ths if statement from preventing this to happen */
 if(stored !== null){
     renderLastRegistered()
 }
+/* clears local storage */
 function removoveCities(){
 localStorage.removeItem("Cities")
 }
 clearHistory.addEventListener("click", removoveCities)
 searchButton.addEventListener("click", displayWeather);
-
-//$(window).on("load",getcitylast);
-//$("#searchClear").on("click",searchClear(e));
 
